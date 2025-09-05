1bd7386d3b9844e48ec02997b0943c7049b1a37a
Vinayak Mhetre <vinayak.mhetre.office@gmail.com>
2025-09-05 11:20:40 +0530

    [Sep-25]: LeetCode 268 solved.

---
+++ org/leetcode/example/arrays/MissingNumber.java
@@ -0,0 +1,50 @@
+/*
+
+            268. Missing Number
+
+Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array.
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+
+Explanation:
+n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
+2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+
+Explanation:
+n = 9 since there are 9 numbers, so all numbers are in the range [0,9].
+8 is the missing number in the range since it does not appear in nums.
+*/
+
+
+package leetcode.example.arrays;
+
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 5};
+        MissingNumber missingNumber = new MissingNumber();
+        int ans = missingNumber.missingNumber(arr);
+        System.out.println("Answer : " + ans);
+    }
+
+    // Logic Method
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int total = len*(len+1)/2;
+        int sum=0;
+        for (int i:nums){
+            sum+=i;
+        }
+        return total-sum;
+    }
+
+}
