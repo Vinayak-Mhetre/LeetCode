2010a2c45452540752a93d605ee0fc30b8a2ebfa
Vinayak Mhetre <vinayak.mhetre.office@gmail.com>
2025-09-15 10:19:53 +0530

    [Sep-25]: LeetCode 1678 solved.

--- org/leetcode/example/string/GoalParserInterpretation.java
+++ org/leetcode/example/string/GoalParserInterpretation.java
@@ -40,19 +40,19 @@ public class GoalParserInterpretation {
 
     // Logic Method
     public String interpret(String command) {
-        String ans="";
+        StringBuilder ans= new StringBuilder();
         for(int i =0; i<command.length();i++){
             if(command.charAt(i)=='G'){
-                ans=ans+"G";
+                ans.append("G");
             }else if(command.charAt(i)=='('){
                 if(command.charAt(i+1)==')'){
-                    ans=ans+"o";
+                    ans.append("o");
                 }else{
-                    ans=ans+"al";
+                    ans.append("al");
                 }
             }
 
         }
-        return ans;
+        return ans.toString();
     }
 }
